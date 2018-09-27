d5dcdeb1694f4c6ab62db6fabb22b595a25439cd
basecamp <cvanderpoel@basecampcodingacademy.org>
2018-09-27 10:46:26 -0500

    For the most part this file is almost complete. All that is left is for me to complete the 'FMDB'

--- objects.js
+++ objects.js
@@ -3,11 +3,11 @@
 // array and each value is the value.
 // Example: [1, 2, 3] -> { "0": 1, "1": 2, "2": 3 }
 function arrayToObject(arr) {
-    var dict = {};
+    var obj = {};
     for (var i = 0; i < arr.length; i++) {
-        dict[i] = arr[i];
+        obj[i] = arr[i];
     }
-    return dict;
+    return obj;
 }
 
 // (String) -> Object
@@ -18,16 +18,78 @@ function arrayToObject(arr) {
 //     - 'charging' - price we're charging
 // The provided string seperates each item with a newline and each piece of an
 // item with a tab.
-function readInventory(inventoryStr) {}
+function readInventory(inventoryStr) {
+    var str = inventoryStr.split("\n");
+    var obj = {};
+    if (inventoryStr.length > 0) {
+        for (var i = 0; i < str.length; i++) {
+            var inv = str[i].split("\t");
+            var item = inv[0];
+            var pay = Number(inv[1]);
+            var charge = Number(inv[2]);
+            obj[item] = {
+                name: item,
+                paid: pay,
+                charging: charge
+            };
+        }
+    } else {
+        return obj;
+    }
+    return obj;
+}
 
 // (Object) -> Bool
 // Takes input in the same format as the output of "readInventory" and returns
 // true if there are no items that cost more than $1.
-function isDollarStore(inventory) {}
+function isDollarStore(inventory) {
+    for (let i of Object.keys(inventory)) {
+        if (inventory[i].price > 1) {
+            return false;
+        }
+    }
+    return true;
+}
 
-function Movie(title, director, genre, cast) {}
+function Movie(title, director, genre, cast) {
+    this.title = title;
+    this.director = director;
+    this.genre = genre;
+    this.cast = cast;
+}
 
-function FMDB(movies) {}
+Movie.prototype.toString = function() {
+    return (
+        "Title: " +
+        this.title +
+        "\n" +
+        "Director: " +
+        this.director +
+        "\n" +
+        "Genre: " +
+        this.genre +
+        "\n" +
+        "Cast:\n- " +
+        this.cast.join("\n- ")
+    );
+};
+Movie.prototype.equals = function(otherMovie) {
+    var ll = require("./arraysAndLoops.js");
+    if (
+        this.title === otherMovie.title &&
+        this.director === otherMovie.director &&
+        this.genre === otherMovie.genre &&
+        ll.arrayEquals(this.cast, otherMovie.cast) === true
+    ) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+function FMDB(movies) {
+    
+}
 
 exports.arrayToObject = arrayToObject;
 exports.readInventory = readInventory;
